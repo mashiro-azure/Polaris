cbb5c8faf6ad8bec73079c7e4ed7de278e7ddec0
mashiro-azure <23091058+mashiro-azure@users.noreply.github.com>
2025-04-22 01:40:31 +0800

    button toggle working on Pin B12
    Set to interrupt, pull-up

---
+++ Core/Inc/screens.h
@@ -0,0 +1,10 @@
+#ifndef SCREENS_H
+#define SCREENS_H
+
+#include <u8g2.h>
+
+typedef enum { SCREEN_MAIN, SCREEN_INFO, SCREEN_LAST } ScreenState;
+
+void screen_draw(u8g2_t oled, ScreenState screen);
+
+#endif
