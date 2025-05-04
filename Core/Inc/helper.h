bda872e9b922fb191ee5d6385ca4a5d7900144fe
mashiro-azure <23091058+mashiro-azure@users.noreply.github.com>
2025-05-05 01:19:17 +0800

    feat: SCREEN_TRACK complete

--- Core/Inc/helper.h
+++ Core/Inc/helper.h
@@ -2,12 +2,23 @@
 #define HELPER_H
 
 #include "screens.h"
+#include "stm32f1xx_hal.h"
+#include "stm32f1xx_hal_adc.h"
 #include <stddef.h>
 #include <stdint.h>
-void generate_uuid(char *uuid, size_t length, uint16_t seed);
+
+typedef struct {
+  double distance;
+  double bearing;
+} DistanceBearing;
+
+void generate_uuid(char *uuid, size_t length, uint32_t seed);
 uint32_t
 get_adc_based_seed(ADC_HandleTypeDef *hadc); // clangd: unknown_typename, idc
 void convert_rawGPS_to_dd_format(SensorState *sensorState, const char *rawLat,
                                  const char *rawLong);
+DistanceBearing calculate_distance_and_bearing(SensorState sensorState,
+                                               const char *targetLatitude,
+                                               const char *targetLongitude);
 
 #endif
