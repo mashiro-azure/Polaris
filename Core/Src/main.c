a5a340a5aeda9aad06568f51538e033be19fdd40
mashiro-azure <23091058+mashiro-azure@users.noreply.github.com>
2025-05-05 22:43:44 +0800

    feat: beep

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -26,6 +26,7 @@
 #include "stm32f103xe.h"
 #include "stm32f1xx_hal.h"
 #include "stm32f1xx_hal_gpio.h"
+#include "stm32f1xx_hal_tim.h"
 #include "u8g2.h"
 #include "u8x8.h"
 #include <stdlib.h>
@@ -52,6 +53,8 @@
 ADC_HandleTypeDef hadc3;
 
 I2C_HandleTypeDef hi2c1;
+
+TIM_HandleTypeDef htim1;
 
 UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart2;
@@ -89,6 +92,13 @@ volatile uint8_t currentMenuItem =
 double distance;
 double bearing;
 
+/*
+  Buzzer
+*/
+volatile uint8_t beepActive = 0; // When 1, the timer ISR toggles the output
+volatile uint32_t lastBeepTime = 0;
+volatile uint32_t beepStartTime = 0;
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -98,6 +108,7 @@ static void MX_I2C1_Init(void);
 static void MX_USART1_UART_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_ADC3_Init(void);
+static void MX_TIM1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -287,6 +298,7 @@ int main(void) {
   MX_USART1_UART_Init();
   MX_USART2_UART_Init();
   MX_ADC3_Init();
+  MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
 
   // Display Setup
@@ -341,6 +353,13 @@ int main(void) {
         currentScreen = SCREEN_MAIN_COORD;
         // Optionally, clear the held flag to avoid re-entering this branch.
         buttonIsHeld = 0;
+
+        sensorState.LORA_Txing = 1;
+
+        beepActive = 1;
+        beepStartTime = now;
+        lastBeepTime = now;
+        HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
       }
     }
 
@@ -390,6 +409,20 @@ int main(void) {
       }
     }
 
+    // Buzzer
+    if (!beepActive && (now - lastBeepTime >= 5000)) {
+      beepActive = 1;
+      beepStartTime = now;
+      lastBeepTime = now;
+      HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+    }
+    // If a beep is active, disable it after 0.5 seconds
+    if (beepActive && (now - beepStartTime >= 500)) {
+      beepActive = 0;
+      HAL_TIM_PWM_Stop(&htim1, TIM_CHANNEL_1);
+    }
+    // Buzzer
+
     screen_idle_check(oled);
     /* USER CODE END WHILE */
 
@@ -403,18 +436,20 @@ int main(void) {
     //     gpsBufferIndex = 0;
     //     gpsBuffer[gpsBufferIndex++] = tempChar;
     //     gpsBufferCapturing = 1;
-    //   } else if (gpsBufferCapturing && gpsBufferIndex < sizeof(gpsBuffer) -
-    //   1) {
+    //   } else if (gpsBufferCapturing && gpsBufferIndex < sizeof(gpsBuffer)
+    //   - 1) {
     //     gpsBuffer[gpsBufferIndex++] = tempChar;
 
     //     if (tempChar == '\n') {
     //       gpsBuffer[gpsBufferIndex] = '\0';
     //       // Only forward if it's a GPRMC sentence
     //       if (strncmp(gpsBuffer, "$GPRMC", 6) == 0) {
-    //         HAL_UART_Transmit(&huart1, (uint8_t *)gpsBuffer, gpsBufferIndex,
+    //         HAL_UART_Transmit(&huart1, (uint8_t *)gpsBuffer,
+    //         gpsBufferIndex,
     //                           HAL_MAX_DELAY);
     //         strncpy(gpsLastSentence, gpsBuffer,
-    //                 sizeof(gpsBuffer)); // copy to gpsLastSetence for further
+    //                 sizeof(gpsBuffer)); // copy to gpsLastSetence for
+    //                 further
     //                                     // processing
     //         gpsLastSentence[sizeof(gpsLastSentence) - 1] = '\0';
     //         processGPSsentence(gpsLastSentence, time, status, latitude,
@@ -455,11 +490,12 @@ void SystemClock_Config(void) {
   /** Initializes the RCC Oscillators according to the specified parameters
    * in the RCC_OscInitTypeDef structure.
    */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL2;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
     Error_Handler();
@@ -470,7 +506,7 @@ void SystemClock_Config(void) {
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
                                 RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV2;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
@@ -559,6 +595,73 @@ static void MX_I2C1_Init(void) {
 }
 
 /**
+ * @brief TIM1 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_TIM1_Init(void) {
+
+  /* USER CODE BEGIN TIM1_Init 0 */
+
+  /* USER CODE END TIM1_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+  TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
+
+  /* USER CODE BEGIN TIM1_Init 1 */
+
+  /* USER CODE END TIM1_Init 1 */
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 7;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 166;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+  if (HAL_TIM_Base_Init(&htim1) != HAL_OK) {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK) {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim1) != HAL_OK) {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK) {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+  sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1) != HAL_OK) {
+    Error_Handler();
+  }
+  sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+  sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+  sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+  sBreakDeadTimeConfig.DeadTime = 0;
+  sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+  sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+  sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+  if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK) {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM1_Init 2 */
+
+  /* USER CODE END TIM1_Init 2 */
+  HAL_TIM_MspPostInit(&htim1);
+}
+
+/**
  * @brief USART1 Initialization Function
  * @param None
  * @retval None
@@ -631,6 +734,7 @@ static void MX_GPIO_Init(void) {
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOE_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
 
